2fcd045cd06fdd289b6bc0e8255ff993ee205cd3
jahidhasan6484 <jahid@lintechgroup.co.uk>
2023-10-03 14:25:48 +0600

    A true classic with a diamond twist added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4e4s5m3cuk8"
+    "revision": "0.vm39riq5ei"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
